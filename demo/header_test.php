9dcfb82655203b12f0778ae1762e0fef64b2348e
millken <millken@localhost.localdomain>
2016-04-13 17:53:33 +0800

    demo use composer

--- demo/header_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$http = new swoole_http_server("127.0.0.1", 9501);
-$http->on('request', function ($request, $response) {
-    //$response->header("Server", "Swoole-Testing");
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-});
-$http->start();
