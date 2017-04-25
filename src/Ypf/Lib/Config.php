f4e710c2e85c7a8c252c8f04e44605899bafe49a
millken <millken@gmail.com>
2017-04-25 09:55:07 +0800

    fix config

--- src/Ypf/Lib/Config.php
+++ src/Ypf/Lib/Config.php
@@ -33,7 +33,7 @@ class Config {
 	public function load($path) {
 		if (is_file($path)) {
 			return self::parseFile($path);
-		}else{
+		}elseif(!is_dir($path)){
 			throw new \Exception(" config path '{$path}' not exist");
 		}
 
