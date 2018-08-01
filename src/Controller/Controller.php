f3489f2a7aa70297ac8f0547d79feec6bb95e0b9
millken <millken@gmail.com>
2018-08-01 06:30:35 +0000

    add simple database connection

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,18 +5,8 @@ declare(strict_types=1);
 namespace Ypf\Controller;
 
 use Psr\Http\Server\MiddlewareInterface;
+use Ypf\Base;
 
-abstract class Controller implements MiddlewareInterface
+abstract class Controller extends Base implements MiddlewareInterface
 {
-    public static $container;
-
-    public function __construct()
-    {
-        static::$container = \Ypf\Container::getContainer();
-    }
-
-    public function __get($name)
-    {
-        return static::$container->get($name);
-    }
 }
